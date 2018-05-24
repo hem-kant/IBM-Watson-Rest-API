516b7b145aa302c501a6fba38207a23612c67d0a
Hem Kant <hemkant.india@gmail.com>
2018-05-24 23:02:12 +0530

    IBM Watson
    IBM Watson Rest API Example

---
+++ IBMWatson/src/IBMWatson/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IBMWatson
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            WatsonConversationHelper helper = new WatsonConversationHelper("<WorkSpace ID>", "<UserID>", "<Password>");
+            var res = helper.GetResponse("hi").GetAwaiter().GetResult();
+        }
+    }
+}
